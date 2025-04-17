97ebc6a1abda942dea3a6b0e3cadc6baa2478998
rowleyup <123601348+rowleyup@users.noreply.github.com>
2025-04-16 23:31:39 -0600

    Update get_ids.py
    Adjusted for tsv

--- StepThreeCode/get_ids.py
+++ StepThreeCode/get_ids.py
@@ -23,7 +23,7 @@ with open(file, "r", encoding="utf-8") as data, open(output, "w", encoding="utf-
         if spot == 0:
             spot += 1
             continue
-        name = line.split(",")[2]
+        name = line.split("\t")[1]
         if spot == 1:
             spot += 1
             out.write(name)
